5fad39e4f5fbd24f5ed4ee8868e616acbbc8d8ee
shafatalitha <talitusapi@gmail.com>
2022-03-24 21:13:03 +0700

    delete form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './index.css';
 import Card from "./Component/card";
-import Form from "./Component/form";
+
 import data from "./data/data";
 function App() {
   return (
     <div className="App">
-      <Form/>
-      <Card img={data.album.images[0].url} title={data.name} artist={data.artists[0].name}/>   
+     
+      <Card img={data.album.images[0].url} title={data.name} artist={data.artists[0].name}/> 
+
     </div>
   );
 }
